2fd377382242b5fe9f9ffba4899be23a580e564b
liwuzhao.kuku <liwuzhao.kuku@bytedance.com>
2019-12-08 22:59:18 +0800

    core.js installed, some common libs updated

---
+++ src/common/libraries/polyfill.js
@@ -0,0 +1,10 @@
+// ployfill imported here
+import 'regenerator-runtime/runtime';
+import 'core-js/features/array/find';
+import 'core-js/features/array/from';
+import 'core-js/features/array/fill';
+import 'core-js/features/promise';
+import 'core-js/features/set-immediate';
+import 'core-js/features/object/set-prototype-of';
+import 'core-js/features/object/assign';
+import 'core-js/features/function/name';
